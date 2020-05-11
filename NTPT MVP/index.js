541a2f418bd0acf5ccba8e446376e3d724759ee2
robin739 <62532532+robin739@users.noreply.github.com>
2020-05-11 14:20:36 +1000

    adding javascript for Journey planner
    add javascript for Journey planner

--- NTPT MVP/index.js
+++ NTPT MVP/index.js
@@ -1,1 +1,27 @@
 
+
+
+var city = ["Darwin", "LacksideDr", "Nightcliff", "Wulagi", "Casurina", "Rosebery", "FannieBay", "Karama"];
+
+var route = ["Chan Ward", "Lyons Ward", "Richardson Ward", "Water ward", "Palmerston", "Litchfield"];
+
+window.onload = function () {
+    debugger;
+    var selectFrom = document.getElementById("ddlFrom");
+    var selectTo = document.getElementById("ddlTo");
+
+    for (var i = 0; i < city.length; i++) {
+        var optionFrom = document.createElement('option');
+        var optionTo = document.createElement('option');
+
+        optionFrom.text = city[i];
+        optionFrom.value = i + 1;
+        selectFrom.add(optionFrom, 0);
+
+        optionTo.text = city[i];
+        optionTo.value = i + 1;
+        selectTo.add(optionTo, 0);
+    }
+
+};
+
