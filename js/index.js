3d4ae1531b2eaf2a94b47d52ac8f6b6bb7e15d2d
Aerika312 <61788148+Aerika312@users.noreply.github.com>
2020-05-12 12:06:41 +1000

    add js for navigation link
    add js for navigation link

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+// variable Initialization
+var city = ["Darwin", "LacksideDr", "Nightcliff", "Wulagi", "Casurina", "Rosebery", "FannieBay", "Karama"];  // For Binding in From location and To location dropdown
+
+var route = ["Chan Ward", "Lyons Ward", "Richardson Ward", "Water ward", "Palmerston", "Litchfield"];        // For Binding in Route dropdown
+
+
+//Bind From Location To Location and Route dropdown on window load event
 window.onload = function () {
     debugger;
     var selectFrom = document.getElementById("ddlFrom");
@@ -26,3 +33,37 @@ window.onload = function () {
         selectRoute.add(optionRoute, 0);
 
     }
+
+};
+
+
+//GotoUrl ---------------------------------------------------------
+function GotoUrl(div) {
+    debugger;
+
+    var elems = document.querySelectorAll(".active");
+
+    [].forEach.call(elems, function (el) {
+        el.classList.remove("active");
+    });
+
+    if (div == "lnkHome") {
+        document.activeElement.classList.add("active");
+        document.getElementById("loadArea").style.display = "none";
+    }
+    else {
+        document.getElementById("loadArea").style.display = "block";
+        var lnkHome = document.getElementById("lnkHome");
+        lnkHome.classList.remove("active");
+
+        //var x = document.activeElement.getAttribute("data-lnk");;
+
+        document.activeElement.parentElement.parentElement.classList.add("active");
+
+        document.getElementById("loadArea").innerHTML = "";
+        document.getElementById("loadArea").innerHTML = document.getElementById(div).innerHTML;
+
+        //document.getElementById("loadArea").innerHTML = '<object type="text/html" data="managingcard.html" ></object>';
+    }
+    return false;
+}
